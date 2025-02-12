0cfcb896a6f5a781c6baab9537e2058c54a67639
brian <parks24@up.edu>
2025-02-11 20:43:03 -0800

    Part A

--- app/src/main/java/up/edu/cs301_homework_1/RainDrop.java
+++ app/src/main/java/up/edu/cs301_homework_1/RainDrop.java
@@ -13,26 +13,19 @@ public class RainDrop {
     protected Paint myPaint; // how the spot is drawn
     protected Random rando = new Random();
 
-    /** gives the spot a random colored paint */
     protected void setRandomPaint() {
         int color = Color.rgb(rando.nextInt(256), rando.nextInt(256), rando.nextInt(256));
         myPaint = new Paint();
         myPaint.setColor(color);
-    }
+    }//setRandomPaint
 
     public RainDrop() {
         x = rando.nextInt(800);
         y = rando.nextInt(800);
         setRandomPaint();
-    }
-
-    public RainDrop(int inX, int inY) {
-        x = inX;
-        y = inY;
-        setRandomPaint();
-    }
+    }//constructor
 
     public void draw(Canvas canvas) {
         canvas.drawCircle(x, y, rad, myPaint);
-    }
+    }//draw
 }//class RainDrop
